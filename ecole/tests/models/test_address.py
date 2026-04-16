a129342d4ebb48a38a3de418a345e5c8e9ab699a
DCluseau <daphne.cluseau@fms-ea.com>
2026-04-16 09:54:30 +0200

    - Added test for __str__ method of Address class - Added __init__ files (otherwise, tests execution doesn't work)

--- ecole/tests/models/test_address.py
+++ ecole/tests/models/test_address.py
@@ -3,5 +3,11 @@
 """
 Address class tests
 """
+import pytest
 
 from ecole.models.address import Address
+
+def test_should_display_object():
+    sut = Address("chemin de la Butte", "Toulouse", 31400)
+    expected_value = "chemin de la Butte, 31400 Toulouse"
+    assert sut.__str__() == expected_value
