2997bebdab8cd019993a9665e2c5dd10568676ed
DCluseau <daphne.cluseau@fms-ea.com>
2026-04-16 10:17:10 +0200

    Added two tests : - Setting student - __str__ method

--- ecole/tests/models/test_course.py
+++ ecole/tests/models/test_course.py
@@ -9,9 +9,23 @@ from datetime import date
 
 from ecole.models.course import Course
 from ecole.models.teacher import Teacher
+from ecole.models.student import Student
 
 def test_should_set_teacher():
     expected_value = Teacher("Martial", "Bret", 50, date(2018,9, 1))
     sut = Course("Python", date(2025, 9, 22), date(2026, 4, 10))
     sut.set_teacher(expected_value)
     assert sut.teacher == expected_value
+
+def test_should_set_student():
+    expected_value = Student("Daphné", "Cluseau", 38)
+    sut = Course("Python", date(2025, 9, 22), date(2026, 4, 10))
+    sut.add_student(expected_value)
+    assert sut.students_taking_it[0] == expected_value
+
+def test_should_return_name_dates_teacher():
+    teacher = Teacher("Martial", "Bret", 50, date(2018, 9, 1))
+    sut = Course("Python", date(2025, 9, 22), date(2026, 4, 10))
+    sut.set_teacher(teacher)
+    expected_value = f"Python (2025-09-22 – 2026-04-10),\nenseigné par {teacher}"
+    assert sut.__str__() == expected_value
