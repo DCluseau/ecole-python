edaa8d676abb4e378fa4deb2d60ea7a484288a11
DCluseau <daphne.cluseau@fms-ea.com>
2026-04-16 12:52:37 +0200

    Added tests for School class : - Add course - Add teacher - Add student - Control of number of courses added - Control of number of students added - Control of number of teachers added

--- ecole/tests/business/test_scool.py
+++ ecole/tests/business/test_scool.py
@@ -9,10 +9,45 @@ from datetime import date
 
 from ecole.business.school import School
 from ecole.models.course import Course
+from ecole.models.student import Student
+from ecole.models.teacher import Teacher
+
 
 def test_should_add_course():
     sut = School()
     course = Course("Python", date(2025, 9, 22), date(2026, 4, 10))
     sut.add_course(course)
     expected_value = course
-    assert sut.courses[len(sut.courses) - 1] == expected_value
+    assert sut.courses[0] == expected_value
+
+def test_should_add_teacher():
+    sut = School()
+    teacher = Teacher("Martial", "Bret", 50, date(2018,9, 1))
+    sut.add_teacher(teacher)
+    expected_value = teacher
+    assert sut.teachers[0] == expected_value
+
+def test_should_add_student():
+    sut = School()
+    student = Student("Daphné", "Cluseau", 38)
+    sut.add_student(student)
+    expected_value = student
+    assert sut.students[0] == expected_value
+
+def test_should_have_correct_nb_of_courses():
+    sut = School()
+    sut.init_static()
+    expected_value = 8
+    assert len(sut.courses) == expected_value
+
+def test_should_have_correct_nb_of_students():
+    sut = School()
+    sut.init_static()
+    expected_value = 3
+    assert len(sut.students) == expected_value
+
+def test_should_have_correct_nb_of_teachers():
+    sut = School()
+    sut.init_static()
+    expected_value = 6
+    assert len(sut.teachers) == expected_value
